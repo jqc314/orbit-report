c8a75a75ca608499ab6b9eda9f7c5f880752aa1b
Quinn Cheung <rhsratm@gmail.com>
2020-03-31 19:20:28 -0500

    json fetch done

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -10,5 +10,14 @@ constructor(name: string, type: string, launchDate: string, orbitType: string, o
     this.orbitType = orbitType,
     this.operational = operational,
     this.type = type;
+
+}
+
+shouldShowWarning() {
+    if (this.type.toLowerCase() === 'space debris') {
+        return true;
+    } else {
+        return false;
+    }
 }
 }
